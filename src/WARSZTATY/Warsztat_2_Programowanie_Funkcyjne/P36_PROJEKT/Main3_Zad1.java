cc013db71fb6ea246ebdb04be6537c9b67d85e6a
goro <pawelsnopeksnopek@gmail.com>
2023-06-05 00:45:04 +0200

    Warsztat 2: Projekt Końcowy Streamy: rozwiązanie pierwszego zadania z poziomu trzeciego kosmetyczne zmiany

--- src/WARSZTATY/Warsztat_2_Programowanie_Funkcyjne/P36_PROJEKT/Main3_Zad1.java
+++ src/WARSZTATY/Warsztat_2_Programowanie_Funkcyjne/P36_PROJEKT/Main3_Zad1.java
@@ -2,6 +2,7 @@ package WARSZTATY.Warsztat_2_Programowanie_Funkcyjne.P36_PROJEKT;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,25 +18,24 @@ public class Main3_Zad1 {
 
         TreeMap<Integer, TreeMap<Product.Category, Long>> result1 = produceShopingList.stream()
                 .filter(p -> (1900 + p.getBuyer().getYearOfBirth()) < (CURRENT_YEAR - 50))
-//                .peek(p-> System.out.println(p.getBuyer().getYearOfBirth()))
                 .collect(Collectors.groupingBy(
                                 p -> p.getBuyer().getYearOfBirth(),
                                 TreeMap::new,
                                 Collectors.groupingBy(
                                         p -> p.getProduct().getCategory(),
                                         TreeMap::new,
-                                        Collectors.mapping(p -> p.getQuantity(),
-                                                Collectors.reducing(0L, (l, r) -> l + r)))
+                                        Collectors.mapping(Purchase::getQuantity,
+                                                Collectors.reducing(0L, Long::sum)))
                         )
                 );
 
         var result2 = result1.entrySet().stream()
                 .collect(Collectors.toMap(
-                        e -> e.getKey(),
+                        Map.Entry::getKey,
                         e -> Arrays.stream(Product.Category.values())
                                 .collect(Collectors.toMap(
                                         categoryKey -> e.getValue().getOrDefault(categoryKey, 0L),
-                                        categoryKey -> List.of(categoryKey),
+                                        List::of,
                                         (l, r) -> Stream.concat(l.stream(), r.stream()).collect(Collectors.toList()),
                                         TreeMap::new
                                 )).entrySet().stream().limit(1).collect(Collectors.toList()),
@@ -51,9 +51,6 @@ public class Main3_Zad1 {
 //                            return result;
 //                        },
 
-
-//        result2.entrySet().stream()
-//                        .
 
         PrintingMap.printingMap(result1);
         System.out.println();
