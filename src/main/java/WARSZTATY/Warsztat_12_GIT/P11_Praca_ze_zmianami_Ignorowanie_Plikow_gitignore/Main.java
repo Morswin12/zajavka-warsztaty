afdddf7bdf025386544369cf69360a75f5c9f723
goro <pawelsnopeksnopek@gmail.com>
2023-09-27 22:20:15 +0000

    Warsztat 12 Git że git

---
+++ src/main/java/WARSZTATY/Warsztat_12_GIT/P11_Praca_ze_zmianami_Ignorowanie_Plikow_gitignore/Main.java
@@ -0,0 +1,4 @@
+package WARSZTATY.Warsztat_12_GIT.P11_Praca_ze_zmianami_Ignorowanie_Plikow_gitignore;
+
+public class Main {
+}
---
+++ src/main/java/WARSZTATY/Warsztat_12_GIT/P12_Praca_ze_zmianami_Cofanie_zmian_Revert/Main.java
@@ -0,0 +1,10 @@
+package WARSZTATY.Warsztat_12_GIT.P12_Praca_ze_zmianami_Cofanie_zmian_Revert;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // git revert
+        // git reset
+
+    }
+}
